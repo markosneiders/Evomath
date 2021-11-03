3ae2a202327bd2f91ffffac4669ebb1a624011d0
Zappingmadnnes <roberts.mezals@gmail.com>
2021-11-03 18:54:32 +0200

    Spiced up game screen a bit

--- src/components/AnswerOption/AnwserOption.js
+++ src/components/AnswerOption/AnwserOption.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 const AnwserOption = (props) => {
 	return (
 		<View style={[styles.option, { backgroundColor: props.color }]}>
+			<View
+				style={{ position: "absolute", alignSelf: "flex-start", margin: 15 }}
+			>
+				<MaterialCommunityIcons name={props.icon} size={35} color="white" />
+			</View>
+
 			<Text
 				style={{
 					fontSize: 60,
@@ -26,6 +32,7 @@ const styles = StyleSheet.create({
 		borderRadius: 25,
 		flex: 1,
 		margin: 10,
+		flexDirection: "row",
 
 		shadowColor: "#000",
 		shadowOffset: {
--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,17 +1,24 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { View, Text, StyleSheet, Animated, Easing } from "react-native";
 import AnwserOption from "../components/AnswerOption/AnwserOption";
 import TouchableScale from "react-native-touchable-scale";
-
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 function GameScreen({ navigation }) {
-	const pgv = useRef(new Animated.Value(1)).current;
+	useEffect(() => {
+		ProgressS();
+		setTimeout(() => setCount(2), 1000);
+		setTimeout(() => setCount(1), 2000);
+		setTimeout(() => setCount(0), 3000);
+	}, []);
+	const pgv = useRef(new Animated.Value(0)).current;
+	const [count, setCount] = useState(3);
 	const Progress = () => {
 		Animated.timing(pgv, {
 			toValue: 0,
 			duration: 3000, //time for bar
 			useNativeDriver: false,
 			easing: Easing.linear,
-		}).start();
+		}).start(() => gameEnd());
 	};
 	const ProgressR = () => {
 		Animated.timing(pgv, {
@@ -21,13 +28,19 @@ function GameScreen({ navigation }) {
 			easing: Easing.linear,
 		}).start();
 	};
+	const ProgressS = () => {
+		Animated.timing(pgv, {
+			toValue: 1,
+			duration: 4000, //time for bar intializing on screen open
+			useNativeDriver: false,
+			easing: Easing.linear,
+		}).start();
+	};
 	const pgw = pgv.interpolate({
-		//logo rotation value interpolation
 		inputRange: [0, 1],
 		outputRange: ["0%", "100%"],
 	});
 	const pgc = pgv.interpolate({
-		//logo rotation value interpolation
 		inputRange: [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 1],
 		outputRange: [
 			"red",
@@ -41,6 +54,9 @@ function GameScreen({ navigation }) {
 			"#32DD2E",
 		],
 	});
+	const gameEnd = () => {
+		navigation.navigate("GameOverScreen");
+	};
 	return (
 		<View style={styles.root}>
 			<View style={styles.questionContainer}>
@@ -54,65 +70,124 @@ function GameScreen({ navigation }) {
 						alignSelf: "flex-start",
 					}}
 				/>
-				<Text
+				<Animated.Text
 					style={{
-						fontSize: 50,
+						fontSize: 70,
 						fontWeight: "700",
+						shadowColor: "#000",
+						shadowOffset: {
+							width: 0,
+							height: 5,
+						},
+						shadowOpacity: 0.25,
+						shadowRadius: 3.84,
+						opacity: 1,
+						color: "white",
 					}}
 				>
-					2 + 2
-				</Text>
+					{count}
+				</Animated.Text>
 			</View>
 			<View style={styles.scoresContainer}>
 				<View style={styles.score}>
-					<Text
+					<View
 						style={{
-							fontSize: 20,
-							fontWeight: "700",
+							shadowColor: "#000",
+							shadowOffset: {
+								width: 0,
+								height: 2,
+							},
+							shadowOpacity: 0.25,
+							shadowRadius: 3.84,
+							marginRight: 10,
+							backgroundColor: "#B22D2D",
+							borderRadius: 100,
+							padding: 5,
 						}}
 					>
-						10
-					</Text>
+						<MaterialCommunityIcons name="account" size={40} color="white" />
+					</View>
+					<View
+						style={{
+							shadowColor: "#000",
+							shadowOffset: {
+								width: 0,
+								height: 2,
+							},
+							shadowOpacity: 0.15,
+							shadowRadius: 3.84,
+						}}
+					>
+						<Text
+							style={{
+								fontSize: 30,
+								fontWeight: "700",
+							}}
+						>
+							4726
+						</Text>
+					</View>
 				</View>
 				<View style={styles.score}>
-					<Text
+					<View
 						style={{
-							fontSize: 20,
-							fontWeight: "700",
+							shadowColor: "#000",
+							shadowOffset: {
+								width: 0,
+								height: 2,
+							},
+							shadowOpacity: 0.25,
+							shadowRadius: 3.84,
+							marginRight: 10,
+							backgroundColor: "#B22D2D",
+							borderRadius: 100,
+							padding: 5,
 						}}
 					>
-						6
-					</Text>
-				</View>
-				<View style={styles.score}>
-					<Text
+						<MaterialCommunityIcons name="trophy" size={40} color="white" />
+					</View>
+					<View
 						style={{
-							fontSize: 20,
-							fontWeight: "700",
+							shadowColor: "#000",
+							shadowOffset: {
+								width: 0,
+								height: 2,
+							},
+							shadowOpacity: 0.15,
+							shadowRadius: 3.84,
 						}}
 					>
-						4
-					</Text>
+						<Text
+							style={{
+								fontSize: 30,
+								fontWeight: "700",
+							}}
+						>
+							5484
+						</Text>
+					</View>
 				</View>
 			</View>
-			<View style={styles.optionsContainer}>
-				<TouchableScale style={{ flex: 1 }} onPress={() => Progress()}>
-					<AnwserOption text="1" color="#B22D2D" />
-				</TouchableScale>
-				<TouchableScale style={{ flex: 1 }} onPress={() => ProgressR()}>
-					<AnwserOption text="2" color="#5240C0" />
-				</TouchableScale>
-			</View>
-			<View style={[styles.optionsContainer, { marginBottom: 20 }]}>
-				<TouchableScale
-					style={{ flex: 1 }}
-					onPress={() => navigation.navigate("GameOverScreen")}
-				>
-					<AnwserOption text="3" color="#FEC601" />
-				</TouchableScale>
-				<TouchableScale style={{ flex: 1 }}>
-					<AnwserOption text="4" color="#48A646" />
-				</TouchableScale>
+			<View style={{ flex: 267 * 2, marginBottom: 20 }}>
+				<View style={styles.optionsContainer}>
+					<TouchableScale style={{ flex: 1 }} onPress={() => Progress()}>
+						<AnwserOption text="1" color="#B22D2D" icon="square" />
+					</TouchableScale>
+					<TouchableScale style={{ flex: 1 }} onPress={() => ProgressR()}>
+						<AnwserOption text="2" color="#5240C0" icon="triangle" />
+					</TouchableScale>
+				</View>
+				<View style={[styles.optionsContainer]}>
+					<TouchableScale
+						style={{ flex: 1 }}
+						onPress={() => navigation.navigate("GameOverScreen")}
+					>
+						<AnwserOption text="3" color="#FEC601" icon="star" />
+					</TouchableScale>
+					<TouchableScale style={{ flex: 1 }}>
+						<AnwserOption text="4" color="#48A646" icon="circle" />
+					</TouchableScale>
+				</View>
 			</View>
 		</View>
 	);
@@ -145,9 +220,11 @@ const styles = StyleSheet.create({
 		backgroundColor: "#E5E5E5",
 		justifyContent: "center",
 		alignItems: "center",
+		flexDirection: "row",
 		borderWidth: 1,
 		borderBottomWidth: 2,
 		borderTopWidth: 2,
+		borderColor: "gray",
 	},
 	optionsContainer: {
 		flex: 267,
