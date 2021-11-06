30acd69fe9abd4a306c878f8cc25bcb733228ae7
BigDDendijs <90863966+BigDDendijs@users.noreply.github.com>
2021-11-06 19:23:06 +0200

    hehe
    hehe

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -11,26 +11,27 @@ import SignupScreen from "../screens/SignupScreen";
 import LoginScreen from "../screens/LoginScreen";
 import AccountScreen from "../screens/AccountScreen";
 import GameOverScreen from "../screens/GameOverScreen";
+import EndijaTestesana from "../screens/EndijaTestesana";
 
 const Stack = createStackNavigator();
 
 function HomeNavigator() {
-	const windowHeight = Dimensions.get("window").height;
-	const windowWidth = Dimensions.get("window").width;
-	const config = {
-		animation: "spring",
-		config: {
-			stiffness: 2000,
-			damping: 500,
-			mass: 20,
-			overshootClamping: false,
-			restDisplacementThreshold: 100,
-			restSpeedThreshold: 10,
-		},
-	};
-	return (
-		<Stack.Navigator>
-			{/*<Stack.Screen
+  const windowHeight = Dimensions.get("window").height;
+  const windowWidth = Dimensions.get("window").width;
+  const config = {
+    animation: "spring",
+    config: {
+      stiffness: 2000,
+      damping: 500,
+      mass: 20,
+      overshootClamping: false,
+      restDisplacementThreshold: 100,
+      restSpeedThreshold: 10,
+    },
+  };
+  return (
+    <Stack.Navigator>
+      {/*<Stack.Screen
 				name="LoginScreen"
 				component={LoginScreen}
 				options={{ headerShown: false }}
@@ -40,80 +41,93 @@ function HomeNavigator() {
 				component={SignupScreen}
 				options={{ headerShown: false }}
 			/>*/}
-			<Stack.Screen
-				name="HomeScreen"
-				component={MenuScreen}
-				options={{
-					headerShown: false,
-					gestureEnabled: false,
-					transitionSpec: {
-						open: config,
-						close: config,
-					},
-				}}
-			/>
-			<Stack.Screen
-				name="ModeScreen"
-				component={ModeScreen}
-				options={{
-					headerShown: false,
-					gestureEnabled: true,
-					gestureDirection: "vertical",
-					gestureResponseDistance: windowHeight,
-					cardStyleInterpolator: transitions.down,
-					transitionSpec: {
-						open: config,
-						close: config,
-					},
-				}}
-			/>
-			<Stack.Screen
-				name="GameScreen"
-				component={GameScreen}
-				options={{
-					headerShown: false,
-					gestureEnabled: false,
-					cardStyleInterpolator: transitions.up,
-					transitionSpec: {
-						open: config,
-						close: config,
-					},
-				}}
-			/>
-			<Stack.Screen
-				name="GameOverScreen"
-				component={GameOverScreen}
-				options={{
-					headerShown: false,
-					gestureEnabled: false,
-					cardStyleInterpolator: transitions.fade,
-					transitionSpec: {
-						open: config,
-						close: config,
-					},
-				}}
-			/>
-			<Stack.Screen
-				name="AccountScreen"
-				component={AccountScreen}
-				options={{
-					headerShown: false,
-					gestureEnabled: true,
-					gestureResponseDistance: windowWidth,
-					cardStyleInterpolator: transitions.right,
-					transitionSpec: {
-						open: config,
-						close: config,
-					},
-				}}
-			/>
-		</Stack.Navigator>
-	);
+      <Stack.Screen
+        name="HomeScreen"
+        component={MenuScreen}
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="ModeScreen"
+        component={ModeScreen}
+        options={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: "vertical",
+          gestureResponseDistance: windowHeight,
+          cardStyleInterpolator: transitions.down,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="GameScreen"
+        component={GameScreen}
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+          cardStyleInterpolator: transitions.up,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="GameOverScreen"
+        component={GameOverScreen}
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+          cardStyleInterpolator: transitions.fade,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="EndijaTestesana"
+        component={EndijaTestesana}
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+          cardStyleInterpolator: transitions.fade,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="AccountScreen"
+        component={AccountScreen}
+        options={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureResponseDistance: windowWidth,
+          cardStyleInterpolator: transitions.right,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 export const AppNavigator = () => (
-	//Exports navigator to be imported in App.js
-	<NavigationContainer>
-		<HomeNavigator />
-	</NavigationContainer>
+  //Exports navigator to be imported in App.js
+  <NavigationContainer>
+    <HomeNavigator />
+  </NavigationContainer>
 );
--- src/screens/ModeScreen.js
+++ src/screens/ModeScreen.js
@@ -5,66 +5,67 @@ import TouchableScale from "react-native-touchable-scale";
 import Background from "../components/Background/Background"; //self made component imports
 import CircleButton from "../components/CircleButton/CircleButton";
 function ModeScreen({ navigation }) {
-	const transBack = () => {
-		//when returning to this screen
-		navigation.goBack();
-	};
-	return (
-		<Animated.View style={{ flex: 1, overflow: "hidden" }}>
-			<Background />
-			<View style={styles.optionContainer}>
-				<View style={{ top: "15%", right: "-35%" }}>
-					<CircleButton
-						Icon={"arrow-up"}
-						IconSize={40}
-						TextSize={50}
-						Color={"#AA6373"}
-						Size={70}
-						Action={() => transBack()}
-						Bobble={false}
-					/>
-				</View>
-				<View style={{ top: "20%", right: "19%" }}>
-					<CircleButton
-						Text={"Infinite"}
-						TextSize={50}
-						Color={"#FEC601"}
-						Size={220}
-					/>
-				</View>
-				<View style={{ top: "15%", right: "-19%" }}>
-					<CircleButton
-						Text={"Practice"}
-						TextSize={50}
-						Color={"#48A646"}
-						Size={220}
-					/>
-				</View>
-				<View style={{ top: "10%", right: "19%" }}>
-					<CircleButton
-						Text={"Compe\ntitive"}
-						TextSize={50}
-						Color={"#5240C0"}
-						Size={220}
-					/>
-				</View>
-				<View style={{ top: "0%", right: "-23%" }}>
-					<CircleButton
-						Text={"1v1"}
-						TextSize={50}
-						Color={"#B22D2D"}
-						Size={180}
-					/>
-				</View>
-			</View>
-		</Animated.View>
-	);
+  const transBack = () => {
+    //when returning to this screen
+    navigation.goBack();
+  };
+  return (
+    <Animated.View style={{ flex: 1, overflow: "hidden" }}>
+      <Background />
+      <View style={styles.optionContainer}>
+        <View style={{ top: "15%", right: "-35%" }}>
+          <CircleButton
+            Icon={"arrow-up"}
+            IconSize={40}
+            TextSize={50}
+            Color={"#AA6373"}
+            Size={70}
+            Action={() => transBack()}
+            Bobble={false}
+          />
+        </View>
+        <View style={{ top: "20%", right: "19%" }}>
+          <CircleButton
+            Text={"Infinite"}
+            TextSize={50}
+            Color={"#FEC601"}
+            Size={220}
+          />
+        </View>
+        <View style={{ top: "15%", right: "-19%" }}>
+          <CircleButton
+            Text={"Practice"}
+            TextSize={50}
+            Color={"#48A646"}
+            Size={220}
+          />
+        </View>
+        <View style={{ top: "10%", right: "19%" }}>
+          <CircleButton
+            Text={"Endija\nTestesana"}
+            TextSize={50}
+            Color={"#5240C0"}
+            Size={220}
+            Action={() => navigation.navigate("EndijaTestesana")}
+          />
+        </View>
+        <View style={{ top: "0%", right: "-23%" }}>
+          <CircleButton
+            Text={"1v1"}
+            TextSize={50}
+            Color={"#B22D2D"}
+            Size={180}
+          />
+        </View>
+      </View>
+    </Animated.View>
+  );
 }
 const styles = StyleSheet.create({
-	optionContainer: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
+  optionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 export default ModeScreen;
