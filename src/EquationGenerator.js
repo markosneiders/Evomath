25f19eb24cb0d6b6d32d23ff90efb66a2a086fde
BigDDendijs <90863966+BigDDendijs@users.noreply.github.com>
2021-11-06 21:47:17 +0200

    he he
    nizhevcijs

--- src/EquationGenerator.js
+++ src/EquationGenerator.js
@@ -1,73 +1,133 @@
 //question generators input player rank and correct streak and get question, answer option1, answer option2, answer option3, answer option4, correct answer option
 const range = ([playerRank, correctStreak]) => {
-	//generate max and min, sum and difference
-	const maxNum = Math.round(24 + (1 * correctStreak * playerRank) / 1000);
-	const minNum = 6 - 1 * correctStreak;
-	return [maxNum, minNum];
+  //generate max and min, sum and difference
+  const maxNum = Math.round(24 + (1 * correctStreak * playerRank) / 1000);
+  const minNum = 6 - 1 * correctStreak;
+  return [maxNum, minNum];
+};
+
+const anwserGen = (correctAnwser) => {
+  function gen1() {
+    var temp = Math.floor(
+      Math.random() * (correctAnwser + 10 - correctAnwser + 1 + 1) +
+        correctAnwser +
+        1
+    );
+    console.log(temp);
+    return temp;
+  }
+
+  function gen2() {
+    var temp = Math.floor(
+      Math.random() * (correctAnwser - 1 - correctAnwser - 10 + 1) +
+        correctAnwser -
+        10
+    );
+    console.log(temp);
+    return temp;
+  }
+
+  function gen3() {
+    var temp =
+      Math.floor(
+        Math.random() * (correctAnwser - 1 - correctAnwser - 2) +
+          correctAnwser -
+          2
+      ) + 2;
+    console.log(temp);
+    return temp;
+  }
+
+  function gen4() {
+    var temp =
+      Math.random() * (correctAnwser + 2 - correctAnwser + 1) +
+      correctAnwser +
+      1;
+    console.log(temp);
+    return temp;
+  }
+
+  function random() {
+    var i = Math.floor(Math.random() * 20) % 4;
+    if (i <= 0) return random();
+    return i;
+  }
+  function execute() {
+    var i = random();
+    eval("gen" + i + "()");
+  }
+
+  var anwserA = execute();
+  var anwserB = execute();
+  var anwserC = execute();
+  var anwserD = execute();
+
+  console.log(anwserA, anwserB, anwserC, anwserD);
 };
 
 export function genAddition([playerRank, correctStreak]) {
-	const addition = {};
-	//Gets maxnum and min num from range()
-	const maxNum = range([playerRank, correctStreak])[0];
-	const minNum = range([playerRank, correctStreak])[1];
+  const addition = {};
+  //Gets maxnum and min num from range()
+  const maxNum = range([playerRank, correctStreak])[0];
+  const minNum = range([playerRank, correctStreak])[1];
 
-	//Question generation
-	addition.anwser = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
-	addition.b = Math.floor(Math.random() * addition.anwser);
-	addition.a = addition.anwser - addition.b;
+  //Question generation
+  addition.anwser = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+  addition.b = Math.floor(Math.random() * addition.anwser);
+  addition.a = addition.anwser - addition.b;
 
-	//Formating and return
-	addition.question = addition.a + "+" + addition.b;
-	return [addition.question, null, null, null, null, null];
+  //Formating and return
+  addition.question = addition.a + "+" + addition.b;
+  anwserGen(addition.anwser);
+  return [addition.question, null, null, null, null, null];
 }
 
 export function genSubtraction([playerRank, correctStreak]) {
-	const subtraction = {};
-	//Gets maxnum and min num from range()
-	const maxNum = range([playerRank, correctStreak])[0];
-	const minNum = range([playerRank, correctStreak])[1];
+  const subtraction = {};
+  //Gets maxnum and min num from range()
+  const maxNum = range([playerRank, correctStreak])[0];
+  const minNum = range([playerRank, correctStreak])[1];
 
-	//Question generation
-	subtraction.anwser = Math.floor(
-		Math.random() * (maxNum - minNum + 1) + minNum
-	);
-	subtraction.b = Math.floor(Math.random() * subtraction.anwser);
-	subtraction.a = subtraction.anwser + subtraction.b;
+  //Question generation
+  subtraction.anwser = Math.floor(
+    Math.random() * (maxNum - minNum + 1) + minNum
+  );
+  subtraction.b = Math.floor(Math.random() * subtraction.anwser);
+  subtraction.a = subtraction.anwser + subtraction.b;
 
-	//Formating and return
-	subtraction.question = subtraction.a + "-" + subtraction.b;
-	return [subtraction.question, null, null, null, null, null];
+  //Formating and return
+  subtraction.question = subtraction.a + "-" + subtraction.b;
+  return [subtraction.question, null, null, null, null, null];
 }
 
 export function genMultiplication([playerRank, correctStreak]) {
-	const multiplication = {};
-	//Gets maxnum and min num from range()
-	const maxNum = range([playerRank, correctStreak])[0];
-	const minNum = range([playerRank, correctStreak])[1];
+  const multiplication = {};
+  //Gets maxnum and min num from range()
+  const maxNum = range([playerRank, correctStreak])[0];
+  const minNum = range([playerRank, correctStreak])[1];
 
-	//Question generation
-	multiplication.a = Math.floor(Math.random() * (10 - 2)) + 2;
-	multiplication.b = Math.floor(Math.random() * (10 - 2)) + 2;
-	multiplication.anwser = multiplication.a * multiplication.b;
+  //Question generation
+  multiplication.a = Math.floor(Math.random() * (10 - 2)) + 2;
+  multiplication.b = Math.floor(Math.random() * (10 - 2)) + 2;
+  multiplication.anwser = multiplication.a * multiplication.b;
 
-	//Formating and return
-	multiplication.question = multiplication.a + "*" + multiplication.b;
-	return [multiplication.question, null, null, null, null, null];
+  //Formating and return
+  multiplication.question = multiplication.a + "*" + multiplication.b;
+  return [multiplication.question, null, null, null, null, null];
 }
 
 export function genDivision([playerRank, correctStreak]) {
-	const division = {};
-	//Gets maxnum and min num from range()
-	const maxNum = range([playerRank, correctStreak])[0];
-	const minNum = range([playerRank, correctStreak])[1];
+  const division = {};
+  //Gets maxnum and min num from range()
+  const maxNum = range([playerRank, correctStreak])[0];
+  const minNum = range([playerRank, correctStreak])[1];
 
-	//Question generation
-	division.a = Math.floor(Math.random() * (10 - 2)) + 2;
-	division.b = Math.floor(Math.random() * (10 - 2)) + 2;
-	division.anwser = division.a * division.b;
+  //Question generation
+  division.a = Math.floor(Math.random() * (10 - 2)) + 2;
+  division.b = Math.floor(Math.random() * (10 - 2)) + 2;
+  division.anwser = division.a * division.b;
 
-	//Formating and return
-	division.question = division.anwser + "/" + division.b;
-	return [division.question, null, null, null, null, null];
+  //Formating and return
+  division.question = division.anwser + "/" + division.b;
+  return [division.question, null, null, null, null, null];
 }
--- src/screens/EndijaTestesana.js
+++ src/screens/EndijaTestesana.js
@@ -1,42 +1,44 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import {
-	genAddition,
-	genSubtraction,
-	genMultiplication,
-	genDivision,
+  genAddition,
+  genSubtraction,
+  genMultiplication,
+  genDivision,
 } from "../EquationGenerator";
+
 function EndijaTestesana() {
-	const [problem, setProblem] = useState();
-	return (
-		<View style={styles.root}>
-			<Button
-				title="Gen +"
-				onPress={() => setProblem(genAddition([1000, 1]))}
-			></Button>
-			<Button
-				title="Gen -"
-				onPress={() => setProblem(genSubtraction([1000, 1]))}
-			></Button>
-			<Button
-				title="Gen *"
-				onPress={() => setProblem(genMultiplication([1000, 1]))}
-			></Button>
-			<Button
-				title="Gen /"
-				onPress={() => setProblem(genDivision([1000, 1]))}
-			></Button>
-			<Text>{problem}</Text>
-		</View>
-	);
+  const [problem, setProblem] = useState();
+  return (
+    <View style={styles.root}>
+      <Button
+        title="Gen +"
+        onPress={() => setProblem(genAddition([1000, 1]))}
+      ></Button>
+      <Button
+        title="Gen -"
+        onPress={() => setProblem(genSubtraction([1000, 1]))}
+      ></Button>
+      <Button
+        title="Gen *"
+        onPress={() => setProblem(genMultiplication([1000, 1]))}
+      ></Button>
+      <Button
+        title="Gen /"
+        onPress={() => setProblem(genDivision([1000, 1]))}
+      ></Button>
+      <Text>{problem}</Text>
+      <Text>--------Anwsers-------</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	root: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default EndijaTestesana;
